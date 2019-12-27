27cf662f852297b9a5741974f24429c792f888f2
Ritchie Einstein <ritchie.einstein@gmail.com>
2019-12-27 23:41:14 +0530

    Integrated Image from AssetImage with pubspec.yaml modifications

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ void main() {
         backgroundColor: Colors.blueGrey[400],
         body: Center(
           child: Image(
-            image: NetworkImage('https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+//            image: NetworkImage('https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+              image: AssetImage('images/diamond.png'),
           ),
         ),
       )
